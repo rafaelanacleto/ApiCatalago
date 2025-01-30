341aada7f73611f1b94f6ca72adf4dd3b6b48df9
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2025-01-30 15:59:41 -0300

    Criando a classe deb context

---
+++ ApiCatalago/Context/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using ApiCatalago.Models;
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCatalago.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+        }
+
+
+    }
+}
