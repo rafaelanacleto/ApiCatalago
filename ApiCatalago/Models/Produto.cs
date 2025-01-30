146466dcd53c7c2357f4a4391e6d58c5fdf4c851
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2025-01-30 09:14:42 -0300

    Fix string null na classe Produto

--- ApiCatalago/Models/Produto.cs
+++ ApiCatalago/Models/Produto.cs
@@ -6,7 +6,7 @@
         public string Nome { get; set; } = string.Empty;
         public string Descricao { get; set; } = string.Empty;
         public decimal Preco { get; set; }
-        public string ImagemUrl { get; set; }
+        public string? ImagemUrl { get; set; }
         public float Estoque { get; set; }
         public DateTime DataCadastro { get; set; }
 
