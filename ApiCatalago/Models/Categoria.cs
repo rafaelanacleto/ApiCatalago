4e3e00d1461a279a1cc5b858ff4c84dc0b283d16
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2025-01-30 08:47:54 -0300

    First commit

---
+++ ApiCatalago/Models/Categoria.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiCatalago.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string? ImagemPath { get; set; }
+
+
+    }
+}
---
+++ ApiCatalago/Models/Produto.cs
@@ -0,0 +1,14 @@
+﻿namespace ApiCatalago.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Descricao { get; set; } = string.Empty;
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public float Estoque { get; set; }
+        public DateTime DataCadastro { get; set; }
+
+    }
+}
