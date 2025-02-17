4df4835ea49520819f2cd30baf6ae07a0d4a77e9
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2025-02-17 12:54:21 -0300

    Criando controller Produto com DbContext

--- ApiCatalago/Controllers/ProdutosController.cs
+++ ApiCatalago/Controllers/ProdutosController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ApiCatalago.Context;
+using ApiCatalago.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiCatalago.Controllers
@@ -10,6 +12,18 @@ namespace ApiCatalago.Controllers
     [Route("api/[controller]")]
     public class ProdutosController : ControllerBase
     {
-        
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext contexto)
+        {
+            _context = contexto;
+        }
+
+        [HttpGet] // GET: api/produtos
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            return _context.Produtos.ToList();
+        }
+         
     }
 }
