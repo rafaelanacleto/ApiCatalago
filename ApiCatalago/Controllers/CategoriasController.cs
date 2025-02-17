0b01675b14976f6f22e47062e98bd90ea873ee41
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2025-02-17 17:13:07 -0300

    Seguindo implementação Categorias

--- ApiCatalago/Controllers/CategoriasController.cs
+++ ApiCatalago/Controllers/CategoriasController.cs
@@ -24,5 +24,25 @@ namespace ApiCatalago.Controllers
         {
             return _context.Categorias.ToList();
         }
+
+        [HttpGet("{id}", Name = "ObterCategoria")]
+        public ActionResult<Categoria> Get(int id)
+        {
+            var categoria = _context.Categorias.FirstOrDefault(p => p.Id == id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+            return categoria;
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Categoria categoria)
+        {
+            _context.Categorias.Add(categoria);
+            _context.SaveChanges();
+            return new CreatedAtRouteResult("ObterCategoria", new { id = categoria.Id }, categoria);
+        }
+
     }
 }
