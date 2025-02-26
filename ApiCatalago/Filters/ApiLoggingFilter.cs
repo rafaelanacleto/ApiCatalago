51481627ec159cfda4e5b82cc8bc74e427b1cc04
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2025-02-26 08:09:29 -0300

    Criando a classe de filters

---
+++ ApiCatalago/Filters/ApiLoggingFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ApiCatalago.Filters
+{
+    public class ApiLoggingFilter : IActionFilter
+    {
+        private readonly ILogger<ApiLoggingFilter> _logger;
+
+        public ApiLoggingFilter(ILogger<ApiLoggingFilter> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
