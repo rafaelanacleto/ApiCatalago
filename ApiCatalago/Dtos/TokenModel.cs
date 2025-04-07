261cfb3b7242259cd535eeabc710d3407afc3050
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2025-04-07 20:29:27 -0300

    Adiciona propriedades AccessToken, RefreshToken e Expiration ao TokenModel

--- ApiCatalago/Dtos/TokenModel.cs
+++ ApiCatalago/Dtos/TokenModel.cs
@@ -2,6 +2,8 @@
 {
     public class TokenModel
     {
-
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime Expiration { get; set; }
     }
 }
