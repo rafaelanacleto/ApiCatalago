bbb9bda328bb0e55a8a75ee0ca0d747b6f8f0263
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2025-04-08 13:53:23 -0300

    Adiciona modelos ResponseModel e ITokenService

---
+++ ApiCatalago/Dtos/ResponseModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalago.Dtos
+{
+    public class ResponseModel
+    {
+        public string? Status { get; set; }
+        public string? Message { get; set; }
+
+    }
+}
