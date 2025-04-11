ceddb6161e48e7716c5fc5d336590c79613a76dc
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2025-04-10 21:39:22 -0300

    Adicionar suporte a tokens JWT e implementar ITokenService
    Foi adicionado suporte a tokens JWT com novos namespaces em `ITokenService.cs` e `TokenService.cs`. A interface `ITokenService` agora inclui os métodos `GenerateRefreshToken()` e `GetPrincipalFromExpiredToken(string token, IConfiguration configuration)`. Uma nova classe `TokenService` foi criada para implementar a interface, com métodos que atualmente lançam exceções `NotImplementedException`.

--- ApiCatalago/Services/ITokenService.cs
+++ ApiCatalago/Services/ITokenService.cs
@@ -1,12 +1,19 @@
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace ApiCatalago.Services
 {
     public interface ITokenService
     {
-        
+        JwtSecurityToken GenerateAccessToken(IEnumerable<Claim> claims, IConfiguration configuration);
+
+        string GenerateRefreshToken();
+
+        ClaimsPrincipal GetPrincipalFromExpiredToken(string token, IConfiguration configuration);
+
     }
 }
---
+++ ApiCatalago/Services/TokenService.cs
@@ -0,0 +1,23 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace ApiCatalago.Services
+{
+    public class TokenService : ITokenService
+    {
+        public JwtSecurityToken GenerateAccessToken(IEnumerable<Claim> claims, IConfiguration configuration)
+        {
+            throw new NotImplementedException();
+        }
+        public string GenerateRefreshToken()
+        {
+            throw new NotImplementedException();
+        }
+        public ClaimsPrincipal GetPrincipalFromExpiredToken(string token, IConfiguration configuration)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    {
+    }
+}
