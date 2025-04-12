62b6a2d8a1b107d9a6515653448661c2c1eb7ce2
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2025-04-12 09:26:17 -0300

    Refatora o método GenerateAccessToken para incluir a geração de credenciais e chave de segurança

--- ApiCatalago/Services/TokenService.cs
+++ ApiCatalago/Services/TokenService.cs
@@ -1,6 +1,8 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Security.Cryptography;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ApiCatalago.Services
 {
@@ -10,7 +12,18 @@ namespace ApiCatalago.Services
         {
             
             var key = configuration.GetSection("JWT").GetValue<string>("SecretKey") ?? string.Empty;
-            
+            var privateKey = Encoding.UTF8.GetBytes(key);
+            var securityKey = new SymmetricSecurityKey(privateKey);
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var expiration = DateTime.UtcNow.AddMinutes(30);
+            var token = new JwtSecurityToken(
+                issuer: configuration.GetSection("JWT").GetValue<string>("Issuer"),
+                audience: configuration.GetSection("JWT").GetValue<string>("Audience"),
+                claims: claims,
+                expires: expiration,
+                signingCredentials: credentials
+            );
+            return token;
 
         }
         public string GenerateRefreshToken()
