485969bfeaf15b01f5634cb95add3c03dd2b22b5
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2025-05-10 21:20:42 -0300

    Adiciona configuração de testes para ProdutosUnitTestController
    Inclui namespaces necessários e propriedades para unitOfWork e mapper.
    Adiciona string de conexão e configurações de DbContext.
    Implementa construtores para inicialização adequada do contexto e mapeamento.

--- ApiCatalagoXUnitTest/ProdutosUnitTestController.cs
+++ ApiCatalagoXUnitTest/ProdutosUnitTestController.cs
@@ -3,11 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ApiCatalago.Context;
+using ApiCatalago.Interfaces.Auxiliar;
+using ApiCatalago.Repository;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApiCatalagoXUnitTest
 {   
     public class ProdutosUnitTestController
     {
+        public IDbUnitOfWork unitOfwork;
+
+        public IMapper mapper;
+        public static DbContextOptions<AppDbContext> options { get; set; }
+
+        public static string connectionString = "Server=localhost\\SQLEXPRESS;Database=ApiCatalagoBD;Trusted_Connection=True;TrustServerCertificate=True;";
+
+        static ProdutosUnitTestController()
+        {
+            options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseSqlServer(connectionString)
+                .Options;
+        }
+
+        public ProdutosUnitTestController()
+        {
+            var context = new AppDbContext(options);
+            unitOfwork = new UnitOfWork(context);
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(typeof(ApiCatalago.AutoMapper.MappingProfile));
+            });
+            mapper = config.CreateMapper();
+        }
+
 
     }
 }
